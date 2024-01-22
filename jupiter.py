56035d76e65d053223942aa2ce32fca4d3a49134
Tomas Williams <tomasjwilliams@icloud.com>
2024-01-22 15:32:24 +0000

    creating the moons class and making it possible to access and edit the moons database

--- jupiter.py
+++ jupiter.py
@@ -4,3 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 import numpy as np
+
+# creates a Moons class cotaining multiple methods
+class Moons:
+    
+    # initialises the class
+    def __init__(self):
+        
+        # accesses and reads the database, allowing it to be analysed and manipulated
+        database_service = "sqlite"
+        database = "jupiter.db"
+        connectable = f"{database_service}:///{database}"
+        query = "moons"
+        self.data = pd.read_sql(query, connectable)
+        
+        # changes sets the index of the table to the moon name
+        self.data = self.data.set_index("moon")
